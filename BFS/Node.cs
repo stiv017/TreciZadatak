a518921b25006fbed219244d11b9335797bbee5a
stiv017 <stefantomic017@gmail.com>
2022-08-16 00:53:39 +0200

    V1
    korisceno bfs

---
+++ BFS/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS
+{
+    public class Node
+    {
+        public string data;
+        public Node left,right;
+        public Node(string data, Node left, Node right)
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+        public Node(string data)
+        {
+            this.data=data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
---
+++ BFS/Program.cs
@@ -0,0 +1,34 @@
+﻿using BFS;
+
+static  Node sample_tree()
+{
+    Node root =
+        new Node("A",
+            new Node("B",
+                  new Node("C"), new Node("D")),
+            new Node("E",
+                  new Node("F"), new Node("G", new Node("H"), null)));
+            
+    return root;
+}
+static void bfs_travesal(Node node)
+{
+    Queue<Node> queue = new Queue<Node>();
+    queue.Enqueue(node);
+    while(queue.Count > 0)
+    {
+        node = queue.Dequeue();
+        Console.WriteLine(node.data+"");
+        if(node.left != null)
+        {
+            queue.Enqueue(node.left);
+        }
+        if(node.right != null)
+        {
+            queue.Enqueue(node.right);
+        }
+    }
+}
+Node tree= sample_tree();
+Console.WriteLine("BFS ->"); bfs_travesal(tree);
+
